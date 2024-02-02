cd516dcd632230af8836838d0973eb4dfeba52d4
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-02-02 12:46:58 -0500

    Create function to play one round
    commit will create a function that calls both the user
    and the computers choices and then return the result
    of that match

--- javascript.js
+++ javascript.js
@@ -1,8 +1,25 @@
-//Create a function called getComputerChoice that will randomly return either rock, paper, or scissors
-let computerChoice = ['Rock', 'Paper', 'Scissors'];
+//Create a function that will randomly return either rock, paper, or scissors
+let computerChoice = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
     return computerChoice [(Math.floor(Math.random() * 3))];
 }
+//Create a case insensitive prompt to get users answer
+let userSelection = prompt("Make your selection!", "Rock, Paper, or Scissors?").toLowerCase();
+let computerSelection = getComputerChoice();
 
-let getUserChoice = prompt('Make your selection!', 'Rock, Paper, or Scissors?')
+//Create a function that will call both user and computer choices and return results of that round
+function playRound(userSelection, computerSelection) {
+    if (userSelection === "rock" && computerSelection === "scissors" ||
+        userSelection === "paper" && computerSelection === "rock" ||
+        userSelection === "scissors" && computerSelection === "paper") {
+            return(`You beat his ass! ${userSelection} destroys ${computerSelection}`);
+    } else if (userSelection === "rock" && computerSelection === "paper" ||
+            userSelection === "paper" && computerSelection === "scissors" ||
+            userSelection === "scissors" && computerSelection === "rock") {
+            return(`You got knocked down kid, but you can get back up. ${computerSelection} beats ${userSelection}`);
+    } else if (userSelection === computerSelection){
+            return(`*Cling!* That's the sound of you clashing in a Tie because ${userSelection} and ${computerSelection} are the same!`);
+    } else return(`You must've done something wrong, you fool! ${userSelection} is not valid! Try again!`)
+}
+console.log(playRound(userSelection, computerSelection))
