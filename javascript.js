9d430a362d66783440a3192ab8bf09710d0498ae
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-02-02 13:25:16 -0500

    Create variables to hold user and computer scores
    commit will add variables to hold scores for both user
    and computer as well as add incremental counters in the
    playRound function to add the winners score to their
    scorecard

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 //Create a function that will randomly return either rock, paper, or scissors
 let computerChoice = ['rock', 'paper', 'scissors'];
-
 function getComputerChoice() {
     return computerChoice [(Math.floor(Math.random() * 3))];
 }
@@ -8,18 +7,23 @@ function getComputerChoice() {
 let userSelection = prompt("Make your selection!", "Rock, Paper, or Scissors?").toLowerCase();
 let computerSelection = getComputerChoice();
 
+//create variables to house the user and computers scores
+let userScore = 0;
+let computerScore = 0;
+
 //Create a function that will call both user and computer choices and return results of that round
 function playRound(userSelection, computerSelection) {
     if (userSelection === "rock" && computerSelection === "scissors" ||
         userSelection === "paper" && computerSelection === "rock" ||
-        userSelection === "scissors" && computerSelection === "paper") {
-            return(`You beat his ass! ${userSelection} destroys ${computerSelection}`);
+        userSelection === "scissors" && computerSelection === "paper") { userScore++;
+            return`You beat his ass! ${userSelection} destroys ${computerSelection}`;
     } else if (userSelection === "rock" && computerSelection === "paper" ||
             userSelection === "paper" && computerSelection === "scissors" ||
-            userSelection === "scissors" && computerSelection === "rock") {
-            return(`You got knocked down kid, but you can get back up. ${computerSelection} beats ${userSelection}`);
+            userSelection === "scissors" && computerSelection === "rock") { computerScore++;
+            return`You got knocked down kid, but you can get back up. ${computerSelection} beats ${userSelection}`;
     } else if (userSelection === computerSelection){
-            return(`*Cling!* That's the sound of you clashing in a Tie because ${userSelection} and ${computerSelection} are the same!`);
-    } else return(`You must've done something wrong, you fool! ${userSelection} is not valid! Try again!`)
+            return`*Cling!* That's the sound of you clashing in a Tie because ${userSelection} and ${computerSelection} are the same!`;
+    } else return`You must've done something wrong, you fool! ${userSelection} is not valid! Try again!`;
 }
 console.log(playRound(userSelection, computerSelection))
+console.log(userScore, computerScore)
