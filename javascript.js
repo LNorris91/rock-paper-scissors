1e50423e9b1ab948886a76ddbef0455edff66f38
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-02-02 11:08:02 -0500

    Creates a prompt for user choice
    commit will create a case insensitive prompt for the user to
    submit their choice.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 //Create a function called getComputerChoice that will randomly return either rock, paper, or scissors
-const computerChoice = ['Rock', 'Paper', 'Scissors'];
+let computerChoice = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice() {
     return computerChoice [(Math.floor(Math.random() * 3))];
 }
-console.log(getComputerChoice())
+
+let getUserChoice = prompt('Make your selection!', 'Rock, Paper, or Scissors?')
