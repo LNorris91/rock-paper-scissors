35fb14d813034fc2c4f3aeda70449e59fcb9ccf3
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-02-02 10:54:40 -0500

    Create Function for Computer's Choice
    commit will create an array with computers choices
    along with a function to randomly select one of the
    three choices.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,7 @@
-console.log('Hello, World!')
+//Create a function called getComputerChoice that will randomly return either rock, paper, or scissors
+const computerChoice = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice() {
+    return computerChoice [(Math.floor(Math.random() * 3))];
+}
+console.log(getComputerChoice())
