193bec353fa04290e77bc6aea13208f43bec1642
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-02-15 16:37:30 -0500

    Reformat And Add Score Counter
    this commit adds code to track scores and announce
    a winner at when a player hits 5 points and then
    resets the points back to 0.  It will also Reformat
    the code layout a little without changing it.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 This "Rock, Paper, Scissors" game will be my first real javascript project. I will plan and write out my pseudocode before actually coding anything. I will also try to commit more frequently.  
 
-The aim of this game is to pit user versus computer in a 5 round game of "rock, paper, scissors". Best 3 out of 5 wins.
+The aim of this game is to pit user versus computer in a 5 round game of "rock, paper, scissors". Best 5 out of 9 wins.
--- javascript.js
+++ javascript.js
@@ -2,61 +2,64 @@
 let userScore = 0;
 let computerScore = 0;
 
+const rockBtn = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const result = document.querySelector("#result");
+const yourScore = document.querySelector("#yourScore");
+const theirScore = document.querySelector("#theirScore");
+const finalResult = document.querySelector("#finalResult");
+
+result.style.cssText = "padding-top: 10px; padding-bottom: 10px";
+theirScore.style.cssText = "padding-bottom: 10px";
+
+rockBtn.addEventListener('click', (e) => {
+    userSelection = e.target.value;
+    playGame()});
+paperBtn.addEventListener('click', (e) => {
+    userSelection = e.target.value;
+    playGame()});
+scissorsBtn.addEventListener('click', (e) => {
+    userSelection = e.target.value;
+    playGame()});
+
+function getComputerChoice() {
+    let computerChoices = ['rock', 'paper', 'scissors'];
+    return computerChoices[(Math.floor(Math.random() * 3))];
+}
+
 function playGame() {
-     //Create a function that will randomly return either rock, paper, or scissors
-    let computerChoice = ['rock', 'paper', 'scissors'];
-    function getComputerChoice() {
-        return computerChoice [(Math.floor(Math.random() * 3))];
-    }
-    //let userSelection = prompt("Make your selection!", "Rock, Paper, or Scissors?").toLowerCase();
     let computerSelection = getComputerChoice();
     
-    //Create a function that will call both user and computer choices and return results of that round
-    function playRound(userSelection, computerSelection) {
         if (userSelection === "rock" && computerSelection === "scissors" ||
             userSelection === "paper" && computerSelection === "rock" ||
-            userSelection === "scissors" && computerSelection === "paper") { userScore++;
-                return`You beat his ass! You picked ${userSelection}, which destroys ${computerSelection}`;
+            userSelection === "scissors" && computerSelection === "paper") { 
+                userScore++;
+                yourScore.textContent = `Your Score: ${userScore}`; 
+                theirScore.textContent = `That other Bastard's Score: ${computerScore}`
+                result.textContent = `You beat his ass! You picked ${userSelection}, which destroys their ${computerSelection}`;
         } else if (userSelection === "rock" && computerSelection === "paper" ||
-                userSelection === "paper" && computerSelection === "scissors" ||
-                userSelection === "scissors" && computerSelection === "rock") { computerScore++;
-                return`You got knocked down kid, but you can get back up. Computer picked ${computerSelection}, which beats ${userSelection}`;
+            userSelection === "paper" && computerSelection === "scissors" ||
+            userSelection === "scissors" && computerSelection === "rock") { 
+                computerScore++;
+                yourScore.textContent = `Your Score: ${userScore}`; 
+                theirScore.textContent = `That other Bastard's Score: ${computerScore}`;
+                result.textContent = `You got knocked down kid, but you can get back up. Computer picked ${computerSelection}, which beats your ${userSelection}`;
         } else if (userSelection === computerSelection){
-                return`*Cling!* That's the sound of you clashing in a Tie because you both chose ${userSelection}!`;
-        } else return`You must've done something wrong, you fool! ${userSelection} is not valid! Try again!`;
+                yourScore.textContent = `Your Score: ${userScore}`; 
+                theirScore.textContent = `That other Bastard's Score: ${computerScore}`;
+                result.textContent = `*Cling!* That's the sound of you clashing in a Tie because you both chose ${userSelection}!`;
+        } 
+
+        if (userScore < 5 && computerScore < 5) {
+            finalResult.textContent = ""
+        } else if (userScore === 5) {
+            finalResult.textContent = `Smoked that bitch! You killed 'em ${userScore} times!`;
+            userScore = 0;
+            computerScore = 0;
+        } else if (computerScore === 5) {
+            finalResult.textContent = `Damn! you lost, son. Computer beat you ${computerScore} times.`;
+            userScore = 0;
+            computerScore = 0;
+        } 
     }
-            console.log(playRound(userSelection, computerSelection))
-            console.log (userScore, computerScore)
-}
-
-const rockBtn = document.querySelector("#rock");
-rockBtn.addEventListener('click', (e) => {
-    console.log(e.target.value)
-    userSelection = e.target.value
-    playGame()
-})
-const paperBtn = document.querySelector("#paper");
-paperBtn.addEventListener('click', (e) => {
-    console.log(e.target.value)
-    userSelection = e.target.value
-    playGame()
-})
-const scissorsBtn = document.querySelector("#scissors");
-scissorsBtn.addEventListener('click', (e) => {
-    console.log(e.target.value)
-    userSelection = e.target.value
-    playGame()
-})
-
-//Create a function that displays the final match results
-function matchResults() {
-    if (userScore > computerScore) {
-        return`Smoked that bitch! You killed 'em ${userScore} times!`;
-    } else if (userScore < computerScore) {
-        return `Damn! you lost, son. Computer beat you ${computerScore} times.`;
-    } else if (userScore == computerScore) {
-        return `You both live to fight another day. you tied and both scored ${userScore}`;
-    }
-    console.log(matchResults())
-}
-
